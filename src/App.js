5b78a5c57ed76d2d3e2efa839155c710a1af278f
Alonso Parra <alonsogparra@gmail.com>
2018-11-23 15:03:51 -0400

    Adding Transition on App to show and unshow a Box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import Backdrop from './components/Backdrop/Backdrop';
 import Modal from './components/Modal/Modal';
@@ -7,7 +8,8 @@ import './App.css';
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBox: false
   };
 
   showModal = () => {
@@ -21,10 +23,42 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>React Animations</h1>
+        <button 
+          className="Button"
+          onClick={() => this.setState(prevState => ({ showBox: !prevState.showBox }))}>Toggle Box</button>
+        <br />
+        <br />
+        <Transition
+          mountOnEnter
+          unmountOnExit
+          in={this.state.showBox}
+          timeout={1000}
+          onEnter={() => console.log('Enter')}
+          onEntering={() => console.log('Entering')}
+          onEntered={() => console.log('Entered')}
+          onExit={() => console.log('Exit')}
+          onExiting={() => console.log('Exiting')}
+          onExited={() => console.log('Exited')}>
+
+          {state => (
+            <div
+              style = {{
+                backgroundColor: 'red',
+                width: 100,
+                height:100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1
+              }} 
+            />
+          )}
+        </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         { this.state.modalIsOpen ? (
           <Backdrop show />
         ) : null }
+        <br/>
         <button onClick={this.showModal} className="Button" >Open Modal</button>
       </div>
     );
